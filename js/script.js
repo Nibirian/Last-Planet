f13bd2ce5dc53233c2b8c4323710fd25ee96a9f6
Nibirian <124206832+Nibirian@users.noreply.github.com>
2023-04-06 17:16:48 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,6 +38,26 @@ window.addEventListener("scroll", addShadow);
 
 //=============================================================
 
+const cookieBox = document.querySelector(".cookie-box");
+const cookieBtn = document.querySelector(".cookie-btn");
+
+const showCookie = () => {
+	const cookieEaten = localStorage.getItem("cookie");
+	if (cookieEaten) {
+		cookieBox.classList.add("hide");
+	}
+};
+
+const handleCookieBox = () => {
+	localStorage.setItem("cookie", "true");
+	cookieBox.classList.add("hide");
+};
+
+cookieBtn.addEventListener("click", handleCookieBox);
+showCookie();
+
+//=============================================================
+
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
